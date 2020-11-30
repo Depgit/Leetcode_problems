ef1e7fa2da4db828f7f6c204fd2d216168b2e00b
Deepak Mishra <70760888+Depgit@users.noreply.github.com>
2020-11-30 19:13:00 +0530

    Update zello.cpp

--- zello.cpp
+++ zello.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 
 signed main(){
+  ios_base::sync_with_stdio(0);cin.tie(nullptr);
   int64_t t;cin>>t;
   while(t--){
-    
+    string a,b;cin>>a>>b;cin.ignore();
   }
 }
